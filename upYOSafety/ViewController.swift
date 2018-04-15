c04420b897d23239cfb621b886eaa3275aa79d44
EstefaniaBarron <estefaniabarronbarraza@gmail.com>
2018-04-15 18:49:10 -0400

    added stuff

--- upYOSafety/ViewController.swift
+++ upYOSafety/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func alert(_ sender: Any) {
+        makeAPhoneCall()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,10 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    func makeAPhoneCall() {
+        let url: NSURL = URL(string: "TEL://5165068101")! as NSURL
+        UIApplication.shared.open(url as URL, options: [:], completionHandler: nil)
     }
 
 
